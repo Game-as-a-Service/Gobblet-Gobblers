284bc784486405553f76dd7df0f8dca4ef12b1ac
GrapeHuang <oneheed@gmail.com>
2022-11-16 17:34:53 +0800

    Add Test scenario

---
+++ Gobblet-Gobblers-Test/PlaceActionTests.cs
@@ -0,0 +1,69 @@
+using Gobblet_Gobblers.Enums;
+using Gobblet_Gobblers.Sizes;
+
+namespace Gobblet_Gobblers
+{
+    public class PlaceActionTests
+    {
+        private Checkerboard _checkerboard;
+
+        [SetUp]
+        public void Setup()
+        {
+            _checkerboard = new Checkerboard(3);
+        }
+
+        private static readonly object[] _sourceLists =
+        {
+            new List<Cock> { new Cock(Color.Blue, new Small()) }
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void PlacedSucess()
+        {
+            var placeIndex = 4;
+            var cocks = new List<Cock> { new Cock(Color.Blue, new Small()) };
+            var playersA = new Player()
+                .Nameself("Josh")
+                .AddCocks(cocks);
+
+            _checkerboard.JoinPlayer(playersA);
+            var actual = playersA.GetCock(0);
+
+            _checkerboard.Place(actual, placeIndex);
+
+            var expected = _checkerboard.GetCock(placeIndex);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void PlacedFail()
+        {
+            var placeIndex = 4;
+            var playersA = new Player()
+                .Nameself("Josh");
+
+            _checkerboard.JoinPlayer(playersA);
+            var actual = playersA.GetCock(0);
+
+            _checkerboard.Place(actual, placeIndex);
+
+            var expected = _checkerboard.GetCock(placeIndex);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void PlacedSucess1()
+        {
+            var playersA = new Player()
+                    .Nameself("Josh");
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => playersA.GetCock(0));
+        }
+    }
+}

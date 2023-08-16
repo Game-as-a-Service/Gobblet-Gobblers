7551ca54578f0d2887798aca7b67b949c9f606e2
GrapeHuang <oneheed@gmail.com>
2023-08-17 01:48:18 +0800

    Modify Client BaseAddress

--- src/InterfaceAdapter/Gaas.GobbletGobblers.Client/Program.cs
+++ src/InterfaceAdapter/Gaas.GobbletGobblers.Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5075") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://putaonini.zeabur.app") });
 
 await builder.Build().RunAsync();
